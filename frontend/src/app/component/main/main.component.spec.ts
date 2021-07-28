c758d587f82a7c062bf5d082574a29e1218925f4
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-07-28 18:59:16 +0700

    fix failed test

--- frontend/src/app/component/main/main.component.spec.ts
+++ frontend/src/app/component/main/main.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { SafeHtmlPipe } from 'src/app/pipe/safe-html.pipe';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
@@ -9,7 +9,7 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MainComponent],
+      declarations: [MainComponent, SafeHtmlPipe],
       imports: [RouterTestingModule]
     })
       .compileComponents();
--- frontend/src/app/component/update-status/update-status.component.spec.ts
+++ frontend/src/app/component/update-status/update-status.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SafeHtmlPipe } from 'src/app/pipe/safe-html.pipe';
 import { UpdateStatusComponent } from './update-status.component';
 
 describe('UpdateStatusComponent', () => {
@@ -8,7 +9,7 @@ describe('UpdateStatusComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UpdateStatusComponent],
+      declarations: [UpdateStatusComponent, SafeHtmlPipe],
       imports: [RouterTestingModule]
     })
       .compileComponents();
