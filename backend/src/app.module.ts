3e3142aa35b2c64e4a49cdbe30e11c65b98024a6
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-07-29 22:33:00 +0700

    add users resource skeleton

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,17 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'public'),
+      rootPath: join(__dirname, '..', 'public'),
       exclude: ['/api*'],
     }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
