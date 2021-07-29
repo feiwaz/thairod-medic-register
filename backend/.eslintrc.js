9337f6da417ace3372370c71651d3c6fd5dd1829
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-07-30 02:07:29 +0700

    update backend config files

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
   ],
   root: true,
   env: {
