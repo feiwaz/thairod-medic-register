e0b9baad201fd11d030fb0d1dc5bc69e1ea6eb8e
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-07-31 22:06:23 +0700

    update frontend and readme file

---
+++ frontend/src/app/guard/auth.guard.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
+import { AuthGuard } from './auth.guard';
+
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+
+  const router = { navigate: jasmine.createSpy('navigate') };
+  const authService = jasmine.createSpyObj('AuthenticationService', ['decodeAccessToken', 'isAdmin']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{
+        provide: Router,
+        useValue: router
+      }, {
+        provide: AuthenticationService,
+        useValue: authService
+      }]
+    });
+    guard = TestBed.inject(AuthGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
