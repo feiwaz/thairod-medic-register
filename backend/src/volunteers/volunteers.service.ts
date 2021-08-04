da179468ab5d229483785a333185abe3e4a63395
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-08-04 14:26:10 +0700

    [backend] refactor code

--- backend/src/volunteers/volunteers.service.ts
+++ backend/src/volunteers/volunteers.service.ts
@@ -32,7 +32,7 @@ export class VolunteersService {
       if (error.code === 'ER_DUP_ENTRY') {
         throw new ConflictException('ผู้ใช้นี้ได้ลงทะเบียนแล้ว');
       }
-      throw new InternalServerErrorException(error.code);
+      throw new InternalServerErrorException();
     }
   }
 
