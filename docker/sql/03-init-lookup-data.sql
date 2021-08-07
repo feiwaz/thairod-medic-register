f1d274af55bf38c26e3e195d14774f5e410e5062
Nitipan Pompan <nitipan.p@hotmail.com>
2021-08-07 23:05:05 +0700

    added init script for container (typeorm migration might be difficult for manage horizontal scale of backend in prod.)

---
+++ docker/sql/03-init-lookup-data.sql
@@ -0,0 +1,16 @@
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แอดมินตอบ LINE', 1);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('คัดกรอง', 1);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('ส่งต่อประสานงาน', 0);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('พูดคุยกับผู้ป่วย', 1);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('IT Support', 0);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('จัดซื้อ/หาของ', 0);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('ประสานงานและเอกสาร', 0);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แพทย์อาสา', 1);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แพคและคลัง', 0);
+
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('เวชปฏิบัติทั่วไป');
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('สูตินรีเวช');
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('อายุรกรรม');
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('ศัลยกรรม');
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('กุมารเวช');
+INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('อื่นๆ');
