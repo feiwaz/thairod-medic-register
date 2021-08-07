452df9ee168db4f89c51f93119e10a93854d625f
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-08-08 00:31:04 +0700

    update sql script

--- docker/sql/03-init-lookup-data.sql
+++ docker/sql/03-init-lookup-data.sql
@@ -1,3 +1,5 @@
+INSERT INTO `thairodregdb`.`user` (`id`,`email`,`password`,`firstName`,`lastName`,`contactNumber`,`role`,`isActive`) VALUES ('1', 'admin@admin.com', '$2b$10$Go/Kfd9B6HeZLDtXG3RHGuf1pi83psdqIg5K5KhM7y4E0JC1bcs9.', 'admin', 'thairod', '0123456789', 'admin', '1');
+
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แอดมินตอบ LINE', 1);
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('คัดกรอง', 1);
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('ส่งต่อประสานงาน', 0);
@@ -7,6 +9,8 @@ INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('ประสานงานและเอกสาร', 0);
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แพทย์อาสา', 1);
 INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('แพคและคลัง', 0);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('อบรมอาสาสมัคร', 1);
+INSERT INTO `thairodregdb`.`department` (`label`,`isTrainingRequired`) VALUES ('เฝ้าระวัง', 1);
 
 INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('เวชปฏิบัติทั่วไป');
 INSERT INTO `thairodregdb`.`specialized_field` (`label`) VALUES ('สูตินรีเวช');
