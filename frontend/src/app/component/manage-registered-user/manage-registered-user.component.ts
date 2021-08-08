bbcfab52821cc577e34b076087ec08f3be8b1a6c
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-08-08 11:17:22 +0700

    [frontend] move files

--- frontend/src/app/component/manage-registered-user/manage-registered-user.component.ts
+++ frontend/src/app/component/manage-registered-user/manage-registered-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { BasicInfo } from 'src/app/model/basic-info';
 
 @Component({
   selector: 'app-manage-registered-user',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageRegisteredUserComponent implements OnInit {
 
+  displayedColumns = ['dateTime', 'name', 'status', 'action'];
+  selectedFilterColumn = 'dateTime';
+
+  readonly pageSizeOptions = [6, 16, 30];
+  readonly DOCTOR_COLUMN_MAP: any = {
+    dateTime: 'วันเวลาที่ลงทะเบียน',
+    name: 'ชื่อ-นามสกุล',
+    status: 'สถานะ'
+  };
+
+  isLoading = false;
+  selectColumnOptions: any = [];
+  dataSource = new MatTableDataSource<BasicInfo>();
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
