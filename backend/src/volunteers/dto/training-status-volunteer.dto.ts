ed1da9fa794e6cdcbd4a9c7dfd3ca9969cf013ef
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-08-09 17:12:09 +0700

    [backend] fix folder name and remove unused api

--- backend/src/volunteers/dto/training-status-volunteer.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Department } from '../entities/department.entity';
-
-export class TrainingStatusVolunteerDto {
-    id: number;
-    passedDepartment: Department[];
-    failedDepartment: Department[];
-}
