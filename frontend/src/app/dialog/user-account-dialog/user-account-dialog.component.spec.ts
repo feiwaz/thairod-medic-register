03ed177ae9e33eaf7280881b36624e655a64b136
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-08-12 13:50:54 +0700

    [frontend] resolve test dependencies

--- frontend/src/app/dialog/user-account-dialog/user-account-dialog.component.spec.ts
+++ frontend/src/app/dialog/user-account-dialog/user-account-dialog.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,6 +30,7 @@ describe('UserAccountDialogComponent', () => {
         FormsModule,
         MatProgressBarModule,
         MatDialogModule,
+        MatIconModule,
         BrowserAnimationsModule
       ],
       providers: [{
