f76a4ffb6c84b097f35194bf9e4a9a0b43239e64
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-09-02 01:27:12 +0700

    [frontend] add line liff sdk

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-  ngOnInit(): void {
-    liff.init({ liffId: '1656379037-5YZdvN79' });
-    liff.ready.then(() => {
-      if (!liff.isLoggedIn()) {
-        console.log(liff.login());
-      } else {
-        liff.getProfile().then(profile => {
-          sessionStorage.setItem('lineUserId', profile.userId);
-        });
-      }
-    });
-  }
-
-}
+export class AppComponent { }
--- frontend/src/app/component/main/main.component.ts
+++ frontend/src/app/component/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import liff from '@line/liff';
 import { ImageCachingService } from '../../service/image-caching.service';
 
 @Component({
@@ -20,10 +21,24 @@ export class MainComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.initializeLiffApp();
     this.mainLogo = this.imgCachingService.getImgElement('thairod-logo');
     this.doctorImg = this.imgCachingService.getImgElement('register-doctor');
     this.volImg = this.imgCachingService.getImgElement('register-volunteer');
     this.checkStatusLogo = this.imgCachingService.getImgElement('check-status');
+  }
+
+  private initializeLiffApp() {
+    liff.init({ liffId: '1656379037-5YZdvN79' });
+    liff.ready.then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        liff.getProfile().then(profile => {
+          sessionStorage.setItem('lineUserId', profile.userId);
+        });
+      }
+    });
   }
 
   onRegister(role = 'volunteer'): void {
