79c937f84919abf1eb041748ba869b2e91144bc8
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-09-02 01:23:25 +0700

    [frontend] add line liff sdk

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,12 +10,15 @@ export class AppComponent {
 
   ngOnInit(): void {
     liff.init({ liffId: '1656379037-5YZdvN79' });
-    if (!liff.isLoggedIn()) {
-      console.log(liff.login());
-    } else {
-      liff.getProfile().then(profile =>
-        sessionStorage.setItem('lineUserId', profile.userId));
-    }
+    liff.ready.then(() => {
+      if (!liff.isLoggedIn()) {
+        console.log(liff.login());
+      } else {
+        liff.getProfile().then(profile => {
+          sessionStorage.setItem('lineUserId', profile.userId);
+        });
+      }
+    });
   }
 
 }
