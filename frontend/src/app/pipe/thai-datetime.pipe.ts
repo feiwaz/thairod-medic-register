85dd8a63b9fd3bae71a880bea8759f709dbaa682
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-09-02 19:51:59 +0700

    [frontend] revert last commit

--- frontend/src/app/pipe/thai-datetime.pipe.ts
+++ frontend/src/app/pipe/thai-datetime.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment-timezone';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'thaiDatetime'
@@ -9,7 +9,7 @@ export class ThaiDatetimePipe implements PipeTransform {
   transform(dateTime: string, format: string): string {
     let result = '';
     if (dateTime) {
-      const aMoment = moment(dateTime).locale('th').tz('Asia/Bangkok');
+      const aMoment = moment(dateTime).locale('th');
       const adYear = aMoment.year();
       const beYear = (adYear + 543).toString();
       result = aMoment.format(format).replace(adYear.toString(), beYear);
